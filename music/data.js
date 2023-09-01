e3d27907a13a43f14cf6c9087d4a75070b6b5d05
Happy-soy-sauce <89721857+Happy-soy-sauce@users.noreply.github.com>
2023-09-01 22:01:41 +0800

    Update data.js

--- music/data.js
+++ music/data.js
@@ -1,1 +1,16 @@
-
+let songs = [
+    {
+        name: 'MEGAMIX1',
+        path: 'https://123.ly93.cc/431/2949933/PLUM-MEGAMIX1.ogg',
+        artist: 'PLUM',
+        cover: 'unll',
+        soyid: '001'，
+    },
+    {
+        name: 'MEGAMIX2',
+        path: 'https://123.ly93.cc/431/2949934/PLUM-MEGAMIX2.ogg',
+        artist: 'PLUM',
+        cover: 'unll',
+        soyid：'002'
+    },
+]
