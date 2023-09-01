1c2c8cfa4dbd05224bbb7b7e06e22d8a0fbcad99
Happy-soy-sauce <89721857+Happy-soy-sauce@users.noreply.github.com>
2023-09-01 23:24:24 +0800

    Add files via upload

--- music/data.js
+++ music/data.js
@@ -1,16 +1,50 @@
 let songs = [
     {
-        name: 'MEGAMIX1',
+        name: 'MEGAMIX 1',
         path: 'https://123.ly93.cc/431/2949933/PLUM-MEGAMIX1.ogg',
         artist: 'PLUM',
-        cover: 'unll',
-        soyid: '001'，
+        cover: 'assets/images/cover-1.jpg'
     },
     {
-        name: 'MEGAMIX2',
-        path: 'https://123.ly93.cc/431/2949934/PLUM-MEGAMIX2.ogg',
-        artist: 'PLUM',
-        cover: 'unll',
-        soyid：'002'
+        name: '六兆年と一夜物語',
+        path: 'https://123.ly93.cc/431/2949931/kemu&IA-六兆年と一夜物語 (六兆年零一夜物语).mp3',
+        artist: 'BANGDREAM',
+        cover: 'assets/images/cover-2.jpg'
     },
+    {
+        name: '怪物',
+        path: 'https://123.ly93.cc/431/2949935/PoppinParty - 怪物.mp3',
+        artist: 'Poppin‘Party',
+        cover: 'assets/images/cover-3.jpg'
+    },
+	{
+	    name: '群青',
+	    path: 'https://123.ly93.cc/431/2949938/YOASOBI - 群青.mp3',
+	    artist: 'YOASOBI',
+	    cover: 'assets/images/cover-4.jpg'
+	},
+	{
+	    name: 'Idol',
+	    path: 'https://123.ly93.cc/431/2949937/YOASOBI - Idol.mp3',
+	    artist: 'YOASOBI',
+	    cover: 'assets/images/cover-5.jpg'
+	},
+	{
+	    name: 'MEGAMIX 2',
+	    path: 'https://123.ly93.cc/431/2949934/PLUM-MEGAMIX2.ogg',
+	    artist: 'PLUM',
+	    cover: 'assets/images/cover-6.jpg'
+	},
+	{
+	    name: 'ロキ',
+	    path: 'https://123.ly93.cc/431/2949919/Afterglow - ロキ.mp3',
+	    artist: 'Afterglow',
+	    cover: 'assets/images/cover-7.jpg'
+	},
+	{
+	    name: 'Only My Railgun',
+	    path: 'https://123.ly93.cc/431/2949921/fripSide - Only My Railgun.mp3',
+	    artist: 'fripSide',
+	    cover: 'assets/images/cover-8.jpg'
+	},
 ]
