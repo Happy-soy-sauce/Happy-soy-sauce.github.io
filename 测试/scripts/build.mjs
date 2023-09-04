f9042b3f585aa61af71bd6dff6a674c6092b345c
Happy-soy-sauce <89721857+Happy-soy-sauce@users.noreply.github.com>
2023-09-04 18:50:41 +0800

    Delete 测试 directory

--- 测试/scripts/build.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import { resolve } from 'path';
-import { fileURLToPath } from 'url';
-const dir = fileURLToPath(new URL('.', import.meta.url));
-const source = resolve(dir, '../public');
-const target = resolve(dir, '../dist');
-// fs.rmSync(target, { recursive: true, force: true });
-// fs.mkdirSync(target);
-fs.cpSync(source, target, { recursive: true });
